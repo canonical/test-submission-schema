65c5720dd1dd47435813f1bd1d9062e9b24ab701
Douglas Chiang <douglas.chiang@canonical.com>
2024-12-04 22:41:37 +0000

    Fix spellings and links.
    Signed-off-by: Douglas Chiang <douglas.chiang@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -197,7 +197,8 @@ redirects = {}
 
 linkcheck_ignore = [
     "http://127.0.0.1:8000",
-    "https://github.com/canonical/ACME/*"
+    "https://github.com/canonical/ACME/*",
+    "https://github.com/canonical/test-submission-schema/blob/main/test_submission_schema/v1.json",
 ]
 
 
