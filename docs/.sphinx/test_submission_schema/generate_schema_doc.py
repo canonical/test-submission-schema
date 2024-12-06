415aea6eb192afab2b23e83987f464eeaa11bf1d
Douglas Chiang <douglas.chiang@canonical.com>
2024-12-06 13:27:29 +0000

    Fix version in schema.
    Signed-off-by: Douglas Chiang <douglas.chiang@canonical.com>

--- docs/.sphinx/test_submission_schema/generate_schema_doc.py
+++ docs/.sphinx/test_submission_schema/generate_schema_doc.py
@@ -1,14 +1,24 @@
 from pathlib import Path
 from textwrap import dedent
 import os
+import json
 
-def generate_schema_md(schema_version: str, relative_path: str, target_dir: Path):
-    with open(target_dir / f"test_submission_schema_{schema_version}.md", "w") as md:
+def generate_schema_md(schema_path: Path, target_dir: Path):
+    relative_path = os.path.relpath(schema_path, target_dir)
+    with open(target_dir / f"test_submission_schema_{schema_path.stem}.md", "w") as md:
+        with open(schema_path, "r") as schema_file:
+            schema = json.load(schema_file)
+
+        try:
+            schema_version = schema["properties"]["version"]["const"]
+        except KeyError:
+            raise ValueError("Schema is missing version property")
+
         md.write(dedent(
             f"""
             # Version {schema_version}
 
-            This document describes the schema of the ``test_submission_schema/{schema_version}.json`` 
+            This document describes the schema of the `test_submission_schema/{schema_path.name}` 
             files as part of the submission.
 
             ```{{jsonschema}} {relative_path}
@@ -21,9 +31,8 @@ def main() -> None:
     source_dir = Path(__file__).parent.parent.parent.parent / "test_submission_schema"
     target_dir = Path(__file__).parent.parent.parent / "reference"
     
-    for schema in source_dir.rglob("v*.json"):
-        relative_path = os.path.relpath(schema, target_dir)
-        generate_schema_md(schema.stem, relative_path, target_dir)
+    for schema_path in source_dir.rglob("v*.json"):
+        generate_schema_md(schema_path, target_dir)
 
 
 if __name__ == "__main__":
