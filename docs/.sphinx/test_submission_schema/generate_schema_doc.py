6ed20191a22aae17b24609bc1865b59954224a77
Douglas Chiang <douglas.chiang@canonical.com>
2024-12-10 14:58:17 +0000

    Address PR comments.
    Signed-off-by: Douglas Chiang <douglas.chiang@canonical.com>

--- docs/.sphinx/test_submission_schema/generate_schema_doc.py
+++ docs/.sphinx/test_submission_schema/generate_schema_doc.py
@@ -1,18 +1,31 @@
 from pathlib import Path
 from textwrap import dedent
+from typing import Any
 import os
 import json
+
+def get_keys_to_hide(schema: dict[str, Any]) -> str:
+    has_hide_keys = False
+    hide_keys = []
+
+    if not schema.get("additionalProperties", None):
+        hide_keys.append("/**/additionalProperties")
+        has_hide_keys = True
+    
+    return ":hide_key: " + ",".join(hide_keys) if has_hide_keys else ""
 
 def generate_schema_md(schema_path: Path, target_dir: Path):
     relative_path = os.path.relpath(schema_path, target_dir)
     with open(target_dir / f"test_submission_schema_{schema_path.stem}.md", "w") as md:
         with open(schema_path, "r") as schema_file:
-            schema = json.load(schema_file)
+            schema: dict = json.load(schema_file)
 
         try:
             schema_version = schema["properties"]["version"]["const"]
         except KeyError:
             raise ValueError("Schema is missing version property")
+
+        hide_keys = get_keys_to_hide(schema)
 
         md.write(dedent(
             f"""
@@ -24,6 +37,7 @@ def generate_schema_md(schema_path: Path, target_dir: Path):
             ```{{jsonschema}} {relative_path}
                 :lift_definitions:
                 :auto_reference:
+                {hide_keys}
             """
         ))
 
